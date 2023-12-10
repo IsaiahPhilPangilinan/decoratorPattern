36ac2b4464a3984525af7de9cc000917a0543ef0
Isaiah Phil <126074199+IsaiahPhilPangilinan@users.noreply.github.com>
2023-12-10 22:46:25 +0800

    Adding java files for Decorator Pattern

---
+++ decoratorPattern/BankAccount.java
@@ -0,0 +1,12 @@
+package decoratorPattern;
+
+public interface BankAccount {
+
+    String showAccountType();
+    double getInterestRate();
+    double getBalance();
+    String showBenefits();
+    double computeBalanceWithInterest();
+    String showInfo();
+
+}
---
+++ decoratorPattern/SavingsAccount.java
@@ -0,0 +1,54 @@
+package decoratorPattern;
+
+public class SavingsAccount implements BankAccount{
+
+    private int accountNumber;
+    private String accountName;
+    private double balance;
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "Savings Account";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.01;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String showBenefits() {
+        return "Standard Savings Account";
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return (getInterestRate() * balance) + balance;
+    }
+
+    @Override
+    public String showInfo() {
+        String output = "";
+        output += "Account Number: " + accountNumber +
+                  "\nAccount Name: " + accountName +
+                  "\nBalance: " + balance;
+        return output ;
+    }
+}
